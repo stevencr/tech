b9d86fc667348838a88b9aa6a8722f718cf167d8
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-23 17:36:40 +0100

    refactor: normalize legacy article content

--- src/components/LegacyArticle.tsx
+++ src/components/LegacyArticle.tsx
@@ -1,13 +1,19 @@
 import { useMemo } from 'react';
 
 export function LegacyArticle({ source }: { source: string }) {
-  const html = useMemo(
-    () =>
-      new DOMParser()
-        .parseFromString(source, 'text/html')
-        .querySelector('.article')?.innerHTML ?? '',
-    [source],
-  );
+  const html = useMemo(() => {
+    const document = new DOMParser().parseFromString(source, 'text/html');
+    const article = document.querySelector('.article');
+
+    if (!article) return '';
+
+    article.querySelector('h1')?.remove();
+    article.querySelector('.article__eyebrow')?.remove();
+    article.querySelector('.article__meta')?.remove();
+    article.querySelector('.lead')?.remove();
+
+    return article.innerHTML;
+  }, [source]);
 
   return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
