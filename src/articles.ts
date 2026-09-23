6868bb56bd4a9875d646bb6fea0a4c0ddd366226
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-23 16:02:04 +0100

    Remove invalid TS article registry

--- src/articles.ts
+++
@@ -1,7 +0,0 @@
-import usb from '../articles/usb-serial-communications.html?raw';
-import typescript from '../articles/advanced-typescript.html?raw';
-import { RawArticle } from './RawArticle';
-export const articles=[
- {path:'/articles/usb-serial',title:'USB & Serial Comms',subtitle:'Protocols under the cable',element:<RawArticle source={usb}/>},
- {path:'/articles/advanced-typescript',title:'Advanced TypeScript',subtitle:'The type system under the hood',element:<RawArticle source={typescript}/>}
-] as const;
