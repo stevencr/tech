5b4735eb3fbf97c2bba0ecfe81facfaf8ed0d35a
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-23 17:36:24 +0100

    refactor: remove obsolete article renderer

--- src/RawArticle.tsx
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react';
-import type { ReactNode } from 'react';
-
-export function RawArticle({ source }: { source: string }): ReactNode {
-  const html = useMemo(() => new DOMParser().parseFromString(source, 'text/html').querySelector('.article')?.innerHTML ?? '', [source]);
-  return <article className="article" dangerouslySetInnerHTML={{ __html: html }} />;
-}
