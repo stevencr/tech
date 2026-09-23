554dfdbd17641791d69e021767c74ce4a4b76e3c
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-23 17:30:01 +0100

    Fix HashRouter closing tag in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
-createRoot(document.getElementById('root')!).render(<StrictMode><HashRouter><App /></BrowserRouter></StrictMode>);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
