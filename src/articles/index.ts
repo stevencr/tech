bfa7690756c3185c57a106989be7f916d8cab5ae
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-23 17:44:08 +0100

    Register io_uring article

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,8 +1,10 @@
 import { AdvancedTypeScriptArticle, meta as advancedTypescript } from './advanced-typescript';
 import { UsbSerialArticle, meta as usbSerial } from './usb-serial';
+import { IoUringArticle, meta as ioUring } from './io-uring';
 import type { ArticleMeta } from './types';
 
 export const articles: ArticleMeta[] = [
+  { ...ioUring, component: IoUringArticle },
   { ...usbSerial, component: UsbSerialArticle },
   { ...advancedTypescript, component: AdvancedTypeScriptArticle },
 ];
