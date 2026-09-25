edacef51c3abcaecf3e629066d955bf5b76c0197
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-25 19:45:06 +0100

    Register Docker deep dive

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,10 +1,10 @@
-import { EbpfArticle, meta as ebpf } from './ebpf';
+import { DockerUnderTheHoodArticle, meta as dockerUnderTheHood } from './docker-under-the-hood';
 import { AdvancedTypeScriptArticle, meta as advancedTypescript } from './advanced-typescript';
 import { UsbSerialArticle, meta as usbSerial } from './usb-serial';
 import type { ArticleMeta } from './types';
 
 export const articles: ArticleMeta[] = [
-  { ...ebpf, component: EbpfArticle },
+  { ...dockerUnderTheHood, component: DockerUnderTheHoodArticle },
   { ...usbSerial, component: UsbSerialArticle },
   { ...advancedTypescript, component: AdvancedTypeScriptArticle },
 ];
