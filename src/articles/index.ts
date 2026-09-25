fe006cd609feb4f08df7c70469bde6e6ec0c90b0
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-25 07:54:20 +0100

    Revert eBPF navigation registration

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -2,11 +2,9 @@ import { AdvancedTypeScriptArticle, meta as advancedTypescript } from './advance
 import { UsbSerialArticle, meta as usbSerial } from './usb-serial';
 import { IoUringArticle, meta as ioUring } from './io-uring';
 import { CrdtsArticle, meta as crdts } from './crdts';
-import { EbpfArticle, meta as ebpf } from './ebpf';
 import type { ArticleMeta } from './types';
 
 export const articles: ArticleMeta[] = [
-  { ...ebpf, component: EbpfArticle },
   { ...crdts, component: CrdtsArticle },
   { ...ioUring, component: IoUringArticle },
   { ...usbSerial, component: UsbSerialArticle },
