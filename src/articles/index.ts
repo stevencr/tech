422ec07c6cf535c899cbd9e1d6ed9df99aca9923
Steven Nicholas Cranfield <steven_cranfield@hotmail.com>
2026-09-25 08:05:44 +0100

    Unregister CRDT article

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,11 +1,9 @@
 import { AdvancedTypeScriptArticle, meta as advancedTypescript } from './advanced-typescript';
 import { UsbSerialArticle, meta as usbSerial } from './usb-serial';
 import { IoUringArticle, meta as ioUring } from './io-uring';
-import { CrdtsArticle, meta as crdts } from './crdts';
 import type { ArticleMeta } from './types';
 
 export const articles: ArticleMeta[] = [
-  { ...crdts, component: CrdtsArticle },
   { ...ioUring, component: IoUringArticle },
   { ...usbSerial, component: UsbSerialArticle },
   { ...advancedTypescript, component: AdvancedTypeScriptArticle },
